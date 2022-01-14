004ce4dca796b6f5572a208896d1d1b9438052d4
hoseok.kim <rlaghtjr2@naver.com>
2022-01-14 11:14:30 +0900

    feat: ReviewRepository Test
    #2

---
+++ src/test/java/org/zerock/mreivew/repository/ReviewRepositoryTests.java
@@ -0,0 +1,37 @@
+package org.zerock.mreivew.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mreivew.entity.Member;
+import org.zerock.mreivew.entity.Movie;
+import org.zerock.mreivew.entity.Review;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class ReviewRepositoryTests {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Test
+    public void insertMovieReviews(){
+        IntStream.rangeClosed(1,100).forEach(i->{
+            //영화 번호
+            Long mno = (long)(Math.random()*100)+1;
+            //리뷰어 번호
+            Long mid = ((long)(Math.random()*100)+1);
+            Member member = Member.builder().mid(mid).build();
+
+            Review movieReview = Review.builder()
+                    .member(member)
+                    .movie(Movie.builder().mno(mno).build())
+                    .grade((int)(Math.random()*5)+1)
+                    .text("이 영화에 대한 느낌..."+i)
+                    .build();
+
+            reviewRepository.save(movieReview);
+        });
+    }
+}
