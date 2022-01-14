ec0abed183ab5ddbe7cf385ad5ff1bd28ca21520
hoseok.kim <rlaghtjr2@naver.com>
2022-01-14 09:51:25 +0900

    feat: MovieImage Entity설계
    #1

--- src/main/java/org/zerock/mreivew/entity/Movie.java
+++ src/main/java/org/zerock/mreivew/entity/Movie.java
@@ -1,9 +1,18 @@
 package org.zerock.mreivew.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
 public class Movie extends BaseEntity{
 
     @Id
